e7d17662e54cd0e74ab31c17f68c15be66c26d35
bmnaufaal <bmuhammadopang@gmail.com>
2023-03-09 08:41:59 +0700

    docs: added documentation

--- routers/games.js
+++ routers/games.js
@@ -4,6 +4,5 @@ const gamesRouter = require("express").Router();
 
 gamesRouter.get("/", GamesController.findAll);
 gamesRouter.get("/:id", GamesController.findOne);
-gamesRouter.post("/update", (req, res, next) => {});
 
 module.exports = gamesRouter;
